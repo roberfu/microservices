1c004a4382e39b616ec4e9eb45e09fc789067fa8
Roberto Fuentes <155397372+roberfu@users.noreply.github.com>
2024-07-12 01:00:33 -0400

    Implement eureka server

--- currency-conversion-service/src/main/java/cl/springmachine/microservices/currency/conversion/proxy/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/cl/springmachine/microservices/currency/conversion/proxy/CurrencyExchangeProxy.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import cl.springmachine.microservices.currency.conversion.model.CurrencyConversion;
 
-@FeignClient(name="currency-exchange-service", url="localhost:8000")
+@FeignClient(name="currency-exchange-service")
 public interface CurrencyExchangeProxy {
 	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
